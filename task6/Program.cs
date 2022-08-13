636a3df8a9b91b23cefe02330d4d54359fb57642
Alex <pross.go@gmail.com>
2022-08-13 20:09:18 +0300

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите целое число");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+if (userNumber1%2 == 0)
+{
+ Console.WriteLine("Число "+userNumber1+" четное");
+}
+ else
+{
+Console.WriteLine("Число "+userNumber1+" нечетное ");
+}
