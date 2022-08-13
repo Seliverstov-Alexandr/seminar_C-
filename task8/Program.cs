51fe198460d535f870b3480faddc560d4831a5ec
Alex <pross.go@gmail.com>
2022-08-13 20:48:37 +0300

    task8

---
+++ task8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("введите натуральное число");
+int userNumber1 = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+Console.WriteLine("Четные числа от 1 до "+userNumber1);
+while (count <= userNumber1)
+{
+Console.Write(count+" ");
+count +=2;
+}
+
