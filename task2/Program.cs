ddaf115caf81cb3193ca0623f1a60a6570cf866b
Alex <pross.go@gmail.com>
2022-08-13 19:39:28 +0300

    task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("введите первое число");
+double userNumber1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите второе число");
+double userNumber2 = Convert.ToDouble(Console.ReadLine());
+if (userNumber1>userNumber2)
+{
+ Console.WriteLine("Большее число "+userNumber1+" меньшее число "+userNumber2);
+}
+ else
+{
+Console.WriteLine("Большее число "+userNumber2+" меньшее число "+userNumber1);
+
+}
