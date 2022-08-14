bff221daeaa563a5648d21d2af83ae6a1aa4df66
Alex <pross.go@gmail.com>
2022-08-15 01:39:06 +0300

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет./
+int GetHundredsType (int number)
+{
+    int result = number %1000/100;
+    return result;
+}
+
+    Console.WriteLine("Введите целое число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    if (number<100)
+    {
+       Console.WriteLine("Третьей цифры нет");
+    }
+       else 
+    {  int HundredsType = GetHundredsType (number);
+    Console.WriteLine("Третья цифра числа " + HundredsType);
+    }
+    
